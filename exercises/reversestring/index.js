23f76867980ebbbcdf8da6a8c21ed14f37234a18
Gaurav Mehta <gaurav.mehta509@gmail.com>
2023-01-28 03:56:35 +0530

    implement reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,29 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split('').reduce((reversed, character) => character + reversed, '');
+}
+
+// function reverse(str) {
+//   if (str.length === 1) return str;
+
+//   return reverse(str.slice(1)) + str[0];
+// }
+
+// function reverse(str) {
+//   let reversed = '';
+
+//   // 'hello' => 'h' + '' => 'e' + 'h' => 'l' + 'eh' => 'l' + 'leh' => 'o' + 'lleh' => 'olleh'
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+
+//   return reversed;
+// }
+
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
 
 module.exports = reverse;
