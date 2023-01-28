5a2279609915416384bf2e19a85798455386df4b
Gaurav Mehta <gaurav.mehta509@gmail.com>
2023-01-28 06:39:50 +0530

    implement maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  let max = -1;
+  let maxChar = '';
+
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  for (let key in charMap) {
+    if (charMap[key] > max) {
+      max = charMap[key];
+      maxChar = key;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
