6402bcc595c426dc4ab323b5de0422f83cb8fb45
Gaurav Mehta <gaurav.mehta509@gmail.com>
2023-01-30 00:01:46 +0530

    implement steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,45 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 0, stair = '') {
+  // Construction of all stairs/rows completed
+  if (n === row) {
+    return;
+  }
+
+  // Construction of stair completed
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  // Construct stair
+  stair.length <= row ? (stair += '#') : (stair += ' ');
+
+  // Construct stair recursively
+  steps(n, row, stair);
+}
+
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = '';
+
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+
+//     console.log(stair);
+//   }
+// }
+
+// function steps(n) {
+//   for (let i = 1; i <= n; i++) {
+//     console.log('#'.repeat(i).padEnd(n, ' '));
+//   }
+// }
 
 module.exports = steps;
