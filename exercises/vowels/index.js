00df2168db40e40e137f60e29365a2c6d37269e5
Gaurav Mehta <gaurav.mehta509@gmail.com>
2023-01-30 23:16:52 +0530

    implement vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,20 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  return str.match(/[aeiou]/gi)?.length ?? 0;
+}
+
+// function vowels(str) {
+//   let ans = 0;
+
+//   for (let char of str.toLowerCase()) {
+//     if (['a', 'e', 'i', 'o', 'u'].includes(char)) {
+//       ans++;
+//     }
+//   }
+
+//   return ans;
+// }
 
 module.exports = vowels;
