4e6fc45ff4fccfd05e992cf6d4fd79248a54eab9
Gaurav Mehta <gaurav.mehta509@gmail.com>
2023-01-31 05:45:17 +0530

    implement matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,46 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const matrix = [];
+
+  let startRow = 0;
+  let startColumn = 0;
+  let endRow = n - 1;
+  let endColumn = n - 1;
+  let counter = 1;
+
+  for (let i = 0; i < n; i++) {
+    matrix.push([]);
+  }
+
+  while (startColumn <= endColumn && startRow <= endRow) {
+    for (let i = startColumn; i <= endColumn; i++) {
+      matrix[startRow][i] = counter;
+      counter += 1;
+    }
+    ++startRow;
+
+    for (let i = startRow; i <= endRow; i++) {
+      matrix[i][endColumn] = counter;
+      counter += 1;
+    }
+    --endColumn;
+
+    for (let i = endColumn; i >= startColumn; i--) {
+      matrix[endRow][i] = counter;
+      counter += 1;
+    }
+    --endRow;
+
+    for (let i = endRow; i >= startRow; i--) {
+      matrix[i][startColumn] = counter;
+      counter += 1;
+    }
+    ++startColumn;
+  }
+
+  return matrix;
+}
 
 module.exports = matrix;
