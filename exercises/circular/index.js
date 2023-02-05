b02afc9b16ce9abb0d86a380c4a6083b677fc186
Gaurav Mehta <gaurav.mehta509@gmail.com>
2023-02-05 19:14:49 +0530

    implement circular

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -12,6 +12,25 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+function circular(list) {
+  let slow = list.head;
+  let fast = list.head;
+
+  // Empty list
+  if (list.head === null) {
+    return;
+  }
+
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
+}
 
 module.exports = circular;
