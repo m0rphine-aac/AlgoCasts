31dc339cbe04687bc6fa3107ebff3f431ea2b2b9
Gaurav Mehta <gaurav.mehta509@gmail.com>
2023-02-06 01:24:05 +0530

    implement fromLast

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,6 +11,35 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+  let slow = list.head;
+  let fast = list.head;
+
+  // Empty list
+  if (list.head === null) {
+    return;
+  }
+
+  // Phase 1, make fast n 'nodes' ahead of slow
+  for (let i = 0; i < n; i++) {
+    if (fast.next) {
+      fast = fast.next;
+    } else {
+      return;
+    }
+  }
+
+  // Phase 2, fast is now n 'nodes' ahead
+  while (fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  return slow;
+}
+
+// function fromLast(list, n) {
+//   return list.getAt(list.size() - 1 - n);
+// }
 
 module.exports = fromLast;
