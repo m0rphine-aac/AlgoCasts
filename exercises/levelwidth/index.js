4a48ba718d5f905dea6949868a86825523005f55
Gaurav Mehta <gaurav.mehta509@gmail.com>
2023-02-07 07:39:09 +0530

    implement levelWidth

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,24 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const counters = [0];
+  const queue = [root, 's'];
+
+  while (queue.length) {
+    if (queue[0] === 's' && queue.length === 1) {
+      return counters;
+    }
+
+    if (queue[0] === 's' && queue.length > 1) {
+      queue.push(queue.shift());
+      counters.push(0);
+    }
+
+    const node = queue.shift();
+    queue.push(...node.children);
+    counters[counters.length - 1] += 1;
+  }
+}
 
 module.exports = levelWidth;
